3d9272bbc0fa34f1d05c0ead3ad906e97cbc9639
Alpari0 <adnesspl@gmail.com>
2023-06-30 02:06:41 +0200

    Git PULL/COMMIT from local GIT

---
+++ 1.Formatowanie_czasu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę sekund: ");
+            int sekundy;
+            while (!int.TryParse(Console.ReadLine(), out sekundy) || sekundy < 0)
+            {
+                Console.WriteLine("Niepoprawna wartość. Podaj liczbę nieujemną.");
+                Console.Write("Podaj liczbę sekund: ");
+            }
+
+            FormatujCzas(sekundy);
+        }
+
+        static void FormatujCzas(int sekundy)
+        {
+            int godziny = sekundy / 3600;
+            int minuty = (sekundy % 3600) / 60;
+            int sek = sekundy % 60;
+
+            Console.WriteLine("{0:D2}:{1:D2}:{2:D2}", godziny, minuty, sek);
+        }
---
+++ 2.2.Długość_odcinka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2;
+            Console.Write("Podaj współrzędną x pierwszego punktu: ");
+            while (!double.TryParse(Console.ReadLine(), out x1))
+            {
+                Console.WriteLine("Niepoprawna wartość. Podaj liczbę rzeczywistą.");
+                Console.Write("Podaj współrzędną x pierwszego punktu: ");
+            }
+            Console.Write("Podaj współrzędną y pierwszego punktu: ");
+            while (!double.TryParse(Console.ReadLine(), out y1))
+            {
+                Console.WriteLine("Niepoprawna wartość. Podaj liczbę rzeczywistą.");
+                Console.Write("Podaj współrzędną y pierwszego punktu: ");
+            }
+            Console.Write("Podaj współrzędną x drugiego punktu: ");
+            while (!double.TryParse(Console.ReadLine(), out x2))
+            {
+                Console.WriteLine("Niepoprawna wartość. Podaj liczbę rzeczywistą.");
+                Console.Write("Podaj współrzędną x drugiego punktu: ");
+            }
+            Console.Write("Podaj współrzędną y drugiego punktu: ");
+            while (!double.TryParse(Console.ReadLine(), out y2))
+            {
+                Console.WriteLine("Niepoprawna wartość. Podaj liczbę rzeczywistą.");
+                Console.Write("Podaj współrzędną y drugiego punktu: ");
+            }
+
+            double dlugosc = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+            Console.WriteLine("Długość odcinka wynosi: {0}", dlugosc);
+        }
+
+        static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
